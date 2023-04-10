3741f36f0b6758452baa5d3d4e65599b33678794
nasbko <129598917+nasbko@users.noreply.github.com>
2023-04-10 22:22:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    internal class Program
+    {
+        public static void Main() 
+        { 
+        
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число:");
+            int c = int.Parse(Console.ReadLine());
+
+            double result = (a + b + c) / 3;
+            
+            Console.WriteLine("Среднее арифметическое: " + result);
+        }
+    }
+}
